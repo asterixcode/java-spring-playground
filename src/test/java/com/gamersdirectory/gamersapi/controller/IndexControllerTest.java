9cb094d507b94732a8a8b400437e68ff6bca325c
lucas-saldanha <lk.saldanha@gmail.com>
2022-08-25 13:14:15 +0200

    added index-greeting controller test

---
+++ src/test/java/com/gamersdirectory/gamersapi/controller/IndexControllerTest.java
@@ -0,0 +1,19 @@
+package com.gamersdirectory.gamersapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class IndexControllerTest {
+
+    @Autowired
+    private IndexController controller;
+
+    @Test
+    void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+}
