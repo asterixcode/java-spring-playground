eb7138be0da4c1403528906d66605c377fdf5812
lucas-saldanha <lk.saldanha@gmail.com>
2022-08-30 10:20:14 +0200

    added dirties context to controller test

--- src/test/java/com/gamersdirectory/gamersapi/controller/AccountControllerTest.java
+++ src/test/java/com/gamersdirectory/gamersapi/controller/AccountControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -17,6 +18,7 @@ class AccountControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @DirtiesContext
     @Test
     void shouldSignUpAndSaveAccount() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/account")
