a0d96c972c9e0e85b4fbd7859842f57550f7ffd3
lucas-saldanha <lk.saldanha@gmail.com>
2022-08-30 10:28:59 +0200

    added project application test

---
+++ src/test/java/com/gamersdirectory/gamersapi/GamersApiApplicationTest.java
@@ -0,0 +1,12 @@
+package com.gamersdirectory.gamersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
