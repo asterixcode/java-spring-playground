bdeb91c0bd3fcbce4259833af3d6f05f2e8f0ac2
lucas-saldanha <lk.saldanha@gmail.com>
2022-08-30 10:19:09 +0200

    updated index greeting endpoint test

--- src/test/java/com/gamersdirectory/gamersapi/controller/IndexControllerTest.java
+++ src/test/java/com/gamersdirectory/gamersapi/controller/IndexControllerTest.java
@@ -3,11 +3,21 @@ package com.gamersdirectory.gamersapi.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@DirtiesContext // control the application context cache between tests
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // starts the server with random port to avoid conflicts in test environment
 class IndexControllerTest {
+
+    @LocalServerPort // inject the port
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Autowired
     private IndexController controller;
@@ -15,5 +25,10 @@ class IndexControllerTest {
     @Test
     void contextLoads() {
         assertThat(controller).isNotNull();
+    }
+
+    @Test
+    void greetingShouldReturnDefaultMessage() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test", String.class)).contains("GAMERS API");
     }
 }
