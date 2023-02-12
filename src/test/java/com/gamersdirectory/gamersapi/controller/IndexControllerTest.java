e165af94d3775709987fa2ebfcdeb4273d35785d
lucas.saldanha <lk.saldanha@gmail.com>
2023-02-12 18:19:25 +0100

    added index endpoint test

--- src/test/java/com/gamersdirectory/gamersapi/controller/IndexControllerTest.java
+++ src/test/java/com/gamersdirectory/gamersapi/controller/IndexControllerTest.java
@@ -1,16 +1,24 @@
 package com.gamersdirectory.gamersapi.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DirtiesContext // control the application context cache between tests
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // starts the server with random port to avoid conflicts in test environment
+@AutoConfigureMockMvc
 class IndexControllerTest {
 
     @LocalServerPort // inject the port
@@ -22,6 +30,9 @@ class IndexControllerTest {
     @Autowired
     private IndexController controller;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     void contextLoads() {
         assertThat(controller).isNotNull();
@@ -30,5 +41,16 @@ class IndexControllerTest {
     @Test
     void greetingShouldReturnDefaultMessage() {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test", String.class)).contains("GAMERS API");
+    }
+
+    @Test
+    @DisplayName("Should Return \"GAMERS API\" for /test endpoint")
+    void shouldReturnGreeting_WhenCallingGetEndpoint() throws Exception {
+        MvcResult requestResult = mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().string("GAMERS API")).andReturn();
+
+        // TODO: add assertions
     }
 }
