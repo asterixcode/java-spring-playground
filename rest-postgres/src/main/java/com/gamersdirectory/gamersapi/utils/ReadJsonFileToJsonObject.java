43f8873776ce8d17c69d770201bcb1beb1b65f73
lucas.saldanha <lk.saldanha@gmail.com>
2023-03-12 22:35:01 +0100

    finally working with docker compose

--- rest-postgres/src/main/java/com/gamersdirectory/gamersapi/utils/ReadJsonFileToJsonObject.java
+++ rest-postgres/src/main/java/com/gamersdirectory/gamersapi/utils/ReadJsonFileToJsonObject.java
@@ -1,16 +1,20 @@
 package com.gamersdirectory.gamersapi.utils;
 
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class ReadJsonFileToJsonObject {
 
-    public JSONObject read() throws IOException {
-        String file = "rest-postgres/src/main/resources/openapi/response.json";
-        String content = new String(Files.readAllBytes(Paths.get(file)));
+    @Value("classpath:${openapi.response.filepath}")
+    Resource resourceFile;
+
+    public JSONObject read() {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("openapi/response.json");
+        assert inputStream != null;
+        String content = new Scanner(inputStream).useDelimiter("\\A").next();
         return new JSONObject(content);
     }
 }
